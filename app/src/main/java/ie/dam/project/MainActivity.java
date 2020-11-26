eb8c69b6686807dde2066eb48d216cd3b008a3ab
Andu <stoia.mihai.alexandru@gmail.com>
2020-11-26 20:44:27 +0200

    Bottom Navigation Bar

--- app/src/main/java/ie/dam/project/MainActivity.java
+++ app/src/main/java/ie/dam/project/MainActivity.java
@@ -1,8 +1,16 @@
 package ie.dam.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+import ie.dam.project.fragments.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView=findViewById(R.id.menu_nav_bar);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,
+                    new HomeFragment()).commit();
+                    navView.setSelectedItemId(R.id.menu_home);
+        }
+
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = new HomeFragment();
+                switch (item.getItemId()) {
+                    case R.id.menu_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.menu_filter:
+                        selectedFragment = new FilterFragment();
+                        break;
+                    case R.id.menu_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,
+                        selectedFragment).commit();
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/ie/dam/project/fragments/HomeFragment.java
@@ -0,0 +1,25 @@
+package ie.dam.project.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ie.dam.project.R;
+
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
