32370a04ade8601e4f3af2e4c65b84a6777db4f1
andreitecsor <andrei.tecsor@gmail.com>
2020-11-28 18:52:40 +0200

    Move to StartActivity -> MainActivity

--- app/src/main/java/ie/dam/project/fragments/LoginFragment.java
+++ app/src/main/java/ie/dam/project/fragments/LoginFragment.java
@@ -1,5 +1,6 @@
 package ie.dam.project.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import ie.dam.project.MainActivity;
 import ie.dam.project.R;
 import ie.dam.project.StartActivity;
 
 
 public class LoginFragment extends Fragment {
     private Button registerNowButton;
+    private Button loginButton;
+
+    private TextInputEditText emailTiet;
+    private TextInputEditText passwordTiet;
 
     public LoginFragment() {
     }
@@ -24,7 +33,13 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         initialiseComponents(view);
-        registerNowButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(login());
+        registerNowButton.setOnClickListener(registerNow());
+        return view;
+    }
+
+    private View.OnClickListener registerNow() {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 StartActivity startActivity = (StartActivity) getContext(); //poate returna null. WATCH OUT!!!
@@ -33,11 +48,31 @@ public class LoginFragment extends Fragment {
                             new RegisterFragment()).commit();
                 }
             }
-        });
-        return view;
+        };
+    }
+
+    private View.OnClickListener login() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StartActivity startActivity = (StartActivity) getContext(); //poate returna null. WATCH OUT!!!
+                if (startActivity != null) {
+                    if (emailTiet.getText().toString().equals("a") && passwordTiet.getText().toString().equals("a")) {
+                        Intent intent = new Intent(startActivity, MainActivity.class);
+                        startActivity.startActivity(intent);
+                        startActivity.finish();
+                    } else {
+                        Toast.makeText(startActivity, R.string.start_invalid_credentials, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        };
     }
 
     private void initialiseComponents(View view) {
         registerNowButton = view.findViewById(R.id.frg_login_register_button);
+        loginButton = view.findViewById(R.id.frg_login_button);
+        emailTiet = view.findViewById(R.id.frg_login_email_tiet);
+        passwordTiet = view.findViewById(R.id.frg_login_password_tiet);
     }
 }
