f9e6e6a1d346d780e73d6b8c8489960e6ecf003d
andreitecsor <andrei.tecsor@gmail.com>
2020-11-28 18:28:54 +0200

    RegisterFragment created + switch between Login/Register Fragments

--- app/src/main/java/ie/dam/project/StartActivity.java
+++ app/src/main/java/ie/dam/project/StartActivity.java
@@ -3,17 +3,24 @@ package ie.dam.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import ie.dam.project.fragments.HomeFragment;
 import ie.dam.project.fragments.LoginFragment;
+import ie.dam.project.fragments.RegisterFragment;
 
 public class StartActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        initialiseComponents(savedInstanceState);
+    }
 
+    private void initialiseComponents(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.act_start_frame_layout,
                     new LoginFragment()).commit();
--- app/src/main/java/ie/dam/project/fragments/RegisterFragment.java
+++ app/src/main/java/ie/dam/project/fragments/RegisterFragment.java
@@ -12,25 +12,24 @@ import android.widget.Button;
 import ie.dam.project.R;
 import ie.dam.project.StartActivity;
 
+public class RegisterFragment extends Fragment {
+    private Button loginNowButton;
 
-public class LoginFragment extends Fragment {
-    private Button registerNowButton;
-
-    public LoginFragment() {
+    public RegisterFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
         initialiseComponents(view);
-        registerNowButton.setOnClickListener(new View.OnClickListener() {
+        loginNowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 StartActivity startActivity = (StartActivity) getContext(); //poate returna null. WATCH OUT!!!
                 if (startActivity != null) {
                     startActivity.getSupportFragmentManager().beginTransaction().replace(R.id.act_start_frame_layout,
-                            new RegisterFragment()).commit();
+                            new LoginFragment()).commit();
                 }
             }
         });
@@ -38,6 +37,6 @@ public class LoginFragment extends Fragment {
     }
 
     private void initialiseComponents(View view) {
-        registerNowButton = view.findViewById(R.id.frg_login_register_button);
+        loginNowButton = view.findViewById(R.id.frg_register_login_button);
     }
 }
