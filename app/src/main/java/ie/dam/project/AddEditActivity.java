80dfe0304d2685eda3de8e083c4a2fa567e44da3
andreitecsor <andrei.tecsor@gmail.com>
2020-12-04 23:17:33 +0200

    Updated menu + CurrencyFragment done

---
+++ app/src/main/java/ie/dam/project/AddEditActivity.java
@@ -0,0 +1,14 @@
+package ie.dam.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit);
+    }
+}
--- app/src/main/java/ie/dam/project/MainActivity.java
+++ app/src/main/java/ie/dam/project/MainActivity.java
@@ -1,5 +1,6 @@
 package ie.dam.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import ie.dam.project.fragments.CurrencyFragment;
 import ie.dam.project.fragments.FilterFragment;
 import ie.dam.project.fragments.HomeFragment;
 import ie.dam.project.fragments.ProfileFragment;
@@ -43,6 +45,12 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.menu_home:
                         selectedFragment = new HomeFragment();
                         break;
+                    case R.id.menu_currency:
+                        selectedFragment = new CurrencyFragment();
+                        break;
+                    case R.id.menu_add:
+                        Intent intent = new Intent(getApplicationContext(), AddEditActivity.class);
+                        startActivity(intent);
                     case R.id.menu_filter:
                         selectedFragment = new FilterFragment();
                         break;
