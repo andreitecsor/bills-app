f0f93500f49c525d96de95313cdfbf099ce8ce53
Andu <stoia.mihai.alexandru@gmail.com>
2020-12-22 20:55:14 +0200

    Firebase Authentication DONE + LoginFragment + RegisterFragment implementation

--- app/src/main/java/ie/dam/project/ProfileActivity.java
+++ app/src/main/java/ie/dam/project/ProfileActivity.java
@@ -2,13 +2,24 @@ package ie.dam.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
     }
+
+
 }
