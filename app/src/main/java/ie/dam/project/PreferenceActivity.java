0e807b92cf82f3c36bc363aab19c8816f083f19e
Andu <stoia.mihai.alexandru@gmail.com>
2020-12-29 05:40:47 +0200

    Image database upload DONE

--- app/src/main/java/ie/dam/project/PreferenceActivity.java
+++ app/src/main/java/ie/dam/project/PreferenceActivity.java
@@ -5,9 +5,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -22,17 +24,30 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 
 import ie.dam.project.data.domain.Gender;
@@ -43,7 +58,7 @@ public class PreferenceActivity extends AppCompatActivity {
     public static final String GENDER_KEY = "gender_key";
     public static final String CURRENCY_KEY = "currency_key";
     public static final int IMAGE_CODE = 500;
-    private static final int PICK_IMAGE_CHOOSER_REQUEST_CODE = 300 ;
+    private static final int PICK_IMAGE_CHOOSER_REQUEST_CODE = 300;
 
 
     TextInputEditText nameET;
@@ -54,7 +69,12 @@ public class PreferenceActivity extends AppCompatActivity {
     Button saveBtn;
     Spinner genderSpn;
     SharedPreferences preferences;
-    Uri imageUri;
+    Uri downloadUri = null;
+    Uri uploadUri;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    StorageReference fileReference;
+    Bitmap selectedImage;
 
 
     @Override
@@ -83,7 +103,7 @@ public class PreferenceActivity extends AppCompatActivity {
 
 
                     currentUser.reauthenticate(credential)
-                            .addOnCompleteListener(getOnCompleteListener(name, gender, currency));
+                            .addOnCompleteListener(getOnCompleteListener(name, uploadUri, gender, currency));
                 } else
                     Toast.makeText(getApplicationContext(), "No confirm password", Toast.LENGTH_SHORT).show();
 
@@ -93,19 +113,18 @@ public class PreferenceActivity extends AppCompatActivity {
     }
 
 
-
-
     private void initializeComponents() {
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
         preferences = getSharedPreferences(currentUser.getUid() + RegisterFragment.SHARED_PREF_FILE_EXTENSION, MODE_PRIVATE);
-
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
         currencyEt = findViewById(R.id.act_preference_et_currency);
         genderSpn = findViewById(R.id.act_preference_spinner_sex);
         nameET = findViewById(R.id.act_preference_tiet_name);
         oldPasswordET = findViewById(R.id.act_preference_et_confirm_old_pass);
         saveBtn = findViewById(R.id.act_preference_button_save);
-
         imageIB = findViewById(R.id.act_preference_ib);
+        downloadPhoto();
     }
 
     private void populateSpinner() {
@@ -128,7 +147,7 @@ public class PreferenceActivity extends AppCompatActivity {
         }
     }
 
-    private OnCompleteListener<Void> getOnCompleteListener(String name, String gender, String currency) {
+    private OnCompleteListener<Void> getOnCompleteListener(String name, Uri imageUri, String gender, String currency) {
         return new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
@@ -142,7 +161,7 @@ public class PreferenceActivity extends AppCompatActivity {
                         case 0:
                             break;
                         case 1: {
-                            updateUser(name, null);
+                            updateUser(name, imageUri);
                             addNameToSharedPreferences(name);
                         }
                     }
@@ -218,6 +237,7 @@ public class PreferenceActivity extends AppCompatActivity {
                     .setDisplayName(name)
                     .build();
 
+
             currentUser.updateProfile(profileUpdate)
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
@@ -225,7 +245,7 @@ public class PreferenceActivity extends AppCompatActivity {
                             if (task.isSuccessful()) {
                                 Log.d("FIREBASE", "User profile updated.");
                                 // Toast.makeText(getApplicationContext(), getString(R.string.preference_save_succes), Toast.LENGTH_SHORT).show();
-                            } else Log.d("FIREBASE", task.getException().getMessage());
+                            } //else Log.d("FIREBASE", task.getException().getMessage());
                         }
                     });
         }
@@ -258,16 +278,18 @@ public class PreferenceActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
             try {
-                Uri imageUri = data.getData() ;
+                uploadUri = data.getData();
                 Bitmap bitmap =
-                        MediaStore.Images.Media.getBitmap ( this .getContentResolver() , imageUri) ;
-                imageIB.setImageBitmap( bitmap ) ;
+                        MediaStore.Images.Media.getBitmap(this.getContentResolver(), uploadUri);
+                imageIB.setImageBitmap(bitmap);
                 resizeImage(bitmap);
+                uploadImage();
             } catch (IOException e) {
-                e.printStackTrace() ;
+                e.printStackTrace();
             }
         }
     }
+
     private View.OnClickListener selectPictureFromGallery() {
         return new View.OnClickListener() {
             @Override
@@ -277,16 +299,81 @@ public class PreferenceActivity extends AppCompatActivity {
                 gallery.setType("image/*");
                 gallery.setAction(Intent.ACTION_GET_CONTENT);
 
-                startActivityForResult(gallery,  PICK_IMAGE_CHOOSER_REQUEST_CODE);
+                startActivityForResult(gallery, PICK_IMAGE_CHOOSER_REQUEST_CODE);
 
             }
 
         };
     }
 
-    public void resizeImage (Bitmap bitmap) {
-        Bitmap resized = Bitmap. createScaledBitmap ( bitmap , imageIB.getWidth() , imageIB.getHeight() , true ) ;
-        imageIB .setImageBitmap(resized) ;
+    public void resizeImage(Bitmap bitmap) {
+        Bitmap resized = Bitmap.createScaledBitmap(bitmap, imageIB.getWidth(), imageIB.getHeight(), true);
+        imageIB.setImageBitmap(resized);
+    }
+
+    public void uploadImage() {
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Picture uploading, please wait!");
+        progressDialog.show();
+
+        StorageReference riversRef = storageReference.child("images/" + currentUser.getUid());
+
+        riversRef.putFile(uploadUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        progressDialog.dismiss();
+                        Snackbar.make(findViewById(android.R.id.content), "Image uploaded", Snackbar.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                        Log.d("FAILED", exception.getMessage());
+                        progressDialog.dismiss();
+
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                double progress = (100 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                progressDialog.setMessage("Progress:" + (int) progress + "%");
+            }
+        });
+
+
+//
+    }
+
+    private void downloadPhoto() {
+
+        StorageReference islandRef = storageReference.child("images/" + currentUser.getUid());
+        File localFile = null;
+        try {
+            localFile = File.createTempFile(currentUser.getUid(), "jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String path=localFile.getPath();
+        islandRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+
+                Bitmap bitmap = BitmapFactory.decodeFile(path);
+                resizeImage(bitmap);
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
     }
 }
 
